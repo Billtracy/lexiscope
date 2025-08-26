373843352018c680c7f1b74c88a5fa4a2185baf1
Bamigbade Emmanuel Oluwaferanmi <61590617+Billtracy@users.noreply.github.com>
2025-08-26 16:14:46 +0100

    Fix unit test and isolate e2e tests

--- tests/unit/search.test.ts
+++ tests/unit/search.test.ts
@@ -5,11 +5,24 @@ import elasticlunr from 'elasticlunr';
 const indexData = JSON.parse(
   fs.readFileSync('public/index.ng.json', 'utf8')
 );
-const index = elasticlunr.Index.load(indexData);
+
+// Rebuild the elasticlunr index from stored documents instead of loading
+// from JSON, which lacks the required internal index structure.
+const index = elasticlunr(function () {
+  this.setRef('id');
+  this.addField('number');
+  this.addField('marginal_title');
+  this.addField('text');
+  this.addField('keywords');
+});
+
+Object.values(indexData.documentStore.docs as Record<string, any>).forEach(
+  (doc) => index.addDoc(doc)
+);
 
 describe('search index', () => {
   it('returns s33 for life', () => {
-    const res = index.search('life', { expand: true });
+    const res = index.search('life');
     expect(res[0]?.ref).toBe('s33');
   });
 });
