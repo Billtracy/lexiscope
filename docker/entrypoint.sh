29d8f6593ac343a78a898115c731b745b1db6181
Bill Tracy <billtracy0001@gmail.com>
2026-02-24 14:05:46 +0100

    fix: change storage permissions on container start

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,5 +13,9 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
+echo "Setting directory permissions..."
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
 echo "Starting original command: $@"
 exec "$@"
